de4b2303f5e638960178af55910e5b4e87f36a79
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-10-27 17:45:14 +0100

    Ejercicios 8b_5 y 8b_6 resueltos (PIRÁMIDES)

--- U2_Programacion_Estructurada/PRÁCTICAS/Tarea 8b/src/com/ejercicios16_10_2020/ejercicio8b_5.java
+++ U2_Programacion_Estructurada/PRÁCTICAS/Tarea 8b/src/com/ejercicios16_10_2020/ejercicio8b_5.java
@@ -25,5 +25,40 @@ public class ejercicio8b_5 {
             }
             System.out.println();   //Salto de línea.
         }
+
+        /**Otra posible solución (NO SE LE PIDE EL CARACTER AL USUARIO)**/
+
+        /*
+        int altura, num_espacios;
+        Scanner sc = new Scanner(System.in);
+
+        //Solicito la altura de la pirámide
+        System.out.println("Introduce la altura");
+        altura = sc.nextInt();
+
+        //Calculo el número de espacios de la primera fila
+        num_espacios = altura-1;
+
+        for (int i = 1; i <=altura; i++) {
+
+            //Escribimos los espacios en blanco
+            for (int j=0; j<num_espacios;j++) {
+                System.out.print("-");
+            }
+
+            //Escribimos los asteriscos
+            // La anchura de la pirámide siempre será 2*altura -1
+            // El número de asteriscos será la diferencia de restar a esa anchura
+            // el doble de los espacios en blanco que estoy pintando (a ambos lados de los *)
+            for (int j = 0; j < ((2*altura)-1)- 2*num_espacios; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+            num_espacios--;
+        }
+
+         */
+
     }
 }
--- U2_Programacion_Estructurada/PRÁCTICAS/Tarea 8b/src/com/ejercicios16_10_2020/ejercicio8b_6.java
+++ U2_Programacion_Estructurada/PRÁCTICAS/Tarea 8b/src/com/ejercicios16_10_2020/ejercicio8b_6.java
@@ -4,6 +4,70 @@ import java.util.Scanner;
 
 public class ejercicio8b_6 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tamano, espacios, i, j;
+        String caracter;
 
+        System.out.println("Introduce el Tamaño de la Pirámide:");
+        tamano = sc.nextInt();
+        System.out.println("Introduce el Carácter con el que se pintará la Pirámide:");
+        caracter = sc.next();
+        System.out.println(" ");   //Salto de línea. (Estética de cara al Usuario.)
+
+        espacios = tamano - 1;
+
+        for (i=1 ; i<=tamano ; i++) {
+            for (j=0 ; j<espacios ; j++) {
+                System.out.print(" ");
+            }
+            for (j=0 ; j<((2*tamano)-1)-2*espacios ; j++) {
+                if (j==0 || j==((2*tamano)-1)-2*espacios-1 || i==tamano) {
+                    System.out.print(caracter);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+            espacios--;
+        }
+
+        /** OTRA POSIBLE SOLUCIÓN (no se pide caracter con el que se pintará la pirámide):**/
+        /*
+                int altura, num_espacios;
+        Scanner sc = new Scanner(System.in);
+
+        //Solicito la altura de la pirámide
+        System.out.println("Introduce la altura");
+        altura = sc.nextInt();
+
+        //Calculo el número de espacios de la primera fila
+        num_espacios = altura-1;
+
+        for (int i = 1; i <=altura; i++) {
+
+            //Escribimos los espacios en blanco
+            for (int j=0; j<num_espacios;j++) {
+                System.out.print("-");
+            }
+
+            //Escribimos los asteriscos
+            // La anchura de la pirámide siempre será 2*altura -1
+            // El número de asteriscos será la diferencia de restar a esa anchura
+            // el doble de los espacios en blanco que estoy pintando (a ambos lados de los *)
+            for (int j = 0; j < ((2*altura)-1)- 2*num_espacios; j++) {
+
+                //Pero solo pinto el primero y el último o si es la última fila)
+                if (j==0 || j == ((2*altura)-1)- 2*num_espacios-1 || i==altura) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("-");
+                }
+
+            }
+
+            System.out.println("");
+            num_espacios--;
+        }
+         */
     }
 }
