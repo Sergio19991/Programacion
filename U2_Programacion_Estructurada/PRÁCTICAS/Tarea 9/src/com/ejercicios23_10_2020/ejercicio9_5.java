c566fba4479d69d27fd5f627c5020ec16dd4f3cd
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-10-29 09:55:36 +0100

    Tarea 9 (ejercicio9_5 resuelto)

--- U2_Programacion_Estructurada/PRÁCTICAS/Tarea 9/src/com/ejercicios23_10_2020/ejercicio9_5.java
+++ U2_Programacion_Estructurada/PRÁCTICAS/Tarea 9/src/com/ejercicios23_10_2020/ejercicio9_5.java
@@ -6,10 +6,33 @@ public class ejercicio9_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String frase;
-        String dialecto = "        javalín, javalón";   //8 espacios antes.
-        String no_dialecto = "        javalén, len, len";   //8 espacios antes.
+        String comienzo = "Javalín,javalón";
+        String terminaciones = "javalén,len,len";
 
-        System.out.println("Introduce una Frase:");
+
+        System.out.println("Introduce la frase");
         frase = sc.nextLine();
+
+        if (frase.length() > comienzo.length()) {
+            //Si la frase empieza por lo "Javalín, javalón" o acaba como el dialecto
+            String c = frase.substring(0, comienzo.length());
+            String c2 = frase.substring(frase.length() - terminaciones.length(), frase.length());
+            if (frase.substring(0, comienzo.length()).equals(comienzo)) {
+
+                //Idioma oficial de Javalandia
+                String resultado = frase.substring(comienzo.length(), frase.length()).trim();
+                System.out.println("El mensaje real es:" + resultado);
+
+            } else if (frase.substring(frase.length() - terminaciones.length(), frase.length()).equals(terminaciones)) {
+
+                //Dialecto javalandia
+                String result = frase.substring(0, frase.length() - 1 - terminaciones.length()).trim();
+                System.out.println("El mensaje real es:" + result);
+            } else {
+                System.out.println("Esta frase no es del idioma correcto");
+            }
+        } else {
+            System.out.println("Esta frase no es del idioma correcto");
+        }
     }
 }
