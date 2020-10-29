4ec7a48273452eafbf480d7c869153e6ea6f4201
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-10-29 12:55:06 +0100

    Ejercicio1 resuelto (Entregable_1920)

--- U2_Programacion_Estructurada/PRÁCTICAS/Entregable_1920/src/com/ejercicios27_10_2020/Ej1.java
+++ U2_Programacion_Estructurada/PRÁCTICAS/Entregable_1920/src/com/ejercicios27_10_2020/Ej1.java
@@ -5,32 +5,39 @@ import java.util.Scanner;
 public class Ej1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float primer_control, segundo_control, media;
-        String apto = "apto", no_apto = "no apto", recuperacion;
+        double n1, n2, n3;
+        double media;
+        String recup;
 
         System.out.println("Introduce la Nota del Primer Control:");
-        primer_control = sc.nextInt();
-        System.out.println("Introduce la Nota del Segundo Control:");
-        segundo_control = sc.nextInt();
+        n1 = sc.nextDouble();
+        System.out.println("introduce la Nota del Segundo Control:");
+        n2 = sc.nextDouble();
+        System.out.println("Introduce la Nota del Tercer Control:");
+        n3 = sc.nextDouble();
 
-        media = (primer_control + segundo_control) / 2;
+        media = (n1+n1+n3)/3;
+        System.out.println(media);
 
-        if (media >=5 && media < 6) {
-            System.out.println("Tu nota de Programación es " + media + " - Suficiente");
-        } else if (media >= 6 && media < 7) {
-            System.out.println("Tu nota de Programación es " + media + " - Bien");
-        } else if (media >= 7 && media < 9) {
-            System.out.println("Tu nota de Programación es " + media + " - Notable");
-        } else if (media<5) {
-            System.out.println("¿Cuál ha sido el resultado de la recuperación? (apto/no apto:)");
-            recuperacion = sc.nextLine();
-            if (recuperacion.equals(apto)) {
-                System.out.println("Tu nota de Programación es " + 5 + " - Suficiente");
+        if (media>=5) {
+            if (media>=5 && media<6) {
+                System.out.println("Tu nota de Programación es " + media + " - Suficiente");
+            } else if (media>=6 && media<7) {
+                System.out.println("Tu nota de Programación es " + media + " - Bien");
+            } else if (media>=7 && media<9) {
+                System.out.println("Tu nota de Programación es " + media + " - Notable");
+            } else {
+                System.out.println("Tu nota de Programación es " + media + " - Sobresaliente");
+            }
+        } else {
+            System.out.println("Introduce la Nota de Recuperación:");
+            sc.nextLine();
+            recup = sc.nextLine();
+            if (recup.equals("apto")) {
+                System.out.println("Tu nota de recuperación es 5 - Suficiente");
             } else {
                 System.out.println("Tu nota de Programación es " + media + " - Suspenso");
             }
-        } else {
-            System.out.println("Tu nota de Programación es " + media + " - Sobresaliente");
         }
     }
 }
