974cf81cd202ae2dfd497f83948dfce629927de1
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-11-02 19:50:56 +0100

    Práctica Entregable 2021 (Ejercicio 1 Resuelto)

--- U2_Programacion_Estructurada/PRÁCTICAS/Entregable_2021/src/com/sergiobejaranoarroyo/Ej3.java
+++ U2_Programacion_Estructurada/PRÁCTICAS/Entregable_2021/src/com/sergiobejaranoarroyo/Ej3.java
@@ -1,17 +1,4 @@
 package com.sergiobejaranoarroyo;
 
-import java.util.Scanner;
-
 public class Ej3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String favorito1, favorito2, favorito3, num_loteria;
-
-        System.out.println("Introduzca sus números favoritos:");
-        favorito1 = sc.nextLine();
-        favorito2 = sc.nextLine();
-        favorito3 = sc.nextLine();
-        System.out.println("Introduzca el número de la lotería:");
-        num_loteria = sc.nextLine();
-    }
 }
