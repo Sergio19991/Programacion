e299930ed533459a2f26fa257de7d735a39fdcac
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-11-03 20:58:56 +0100

    Ejercicios1-8 resueltos (Tarea1_Unidad3)

--- U3_Programacion_Modular/PRACTICAS/Tarea 1/src/com/ejercicios03_11_2020/Ejercicio1_1.java
+++ U3_Programacion_Modular/PRACTICAS/Tarea 1/src/com/ejercicios03_11_2020/Ejercicio1_1.java
@@ -1,4 +1,23 @@
 package com.ejercicios03_11_2020;
 
+import java.util.Scanner;
+
 public class Ejercicio1_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.println("Introduce el número de veces:");
+        n = sc.nextInt();
+
+        eco(n);   //Llamada a la función eco();.
+
+    }
+
+    public static void eco(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("Eco...");
+        }
+    }
+
 }
--- U3_Programacion_Modular/PRACTICAS/Tarea 1/src/com/ejercicios03_11_2020/Ejercicio1_4.java
+++ U3_Programacion_Modular/PRACTICAS/Tarea 1/src/com/ejercicios03_11_2020/Ejercicio1_4.java
@@ -1,4 +1,31 @@
 package com.ejercicios03_11_2020;
 
+import java.util.Scanner;
+
 public class Ejercicio1_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.println("Introduce el Primer Número:");
+        num1 = sc.nextInt();
+        System.out.println("Introduce el Segundo Número:");
+        num2 = sc.nextInt();
+
+        System.out.println();
+
+        System.out.print("El Mayor de " + num1 + " y " + num2 + " es el ");
+        System.out.print(mayor2(num1, num2));
+        System.out.println(".");
+
+    }
+
+    public static int mayor2(int num1, int num2) {
+        if (num1 > num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
+
 }
