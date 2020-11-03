d29802b858b9e7a110af1991c8a724e3baae39f1
Sergio1991 <alum.sbejaranoa.2020@iesalixar.org>
2020-11-03 12:39:37 +0100

    Ejercicio 2 Resuelto (Entregable_2021)

--- U2_Programacion_Estructurada/PRÁCTICAS/Entregable_2021/src/com/sergiobejaranoarroyo/Ej2.java
+++ U2_Programacion_Estructurada/PRÁCTICAS/Entregable_2021/src/com/sergiobejaranoarroyo/Ej2.java
@@ -1,4 +1,30 @@
 package com.sergiobejaranoarroyo;
 
+import java.awt.event.MouseAdapter;
+import java.util.Scanner;
+
 public class Ej2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num, numero_final, cifra, digitos_eliminados;
+
+        System.out.println("Introduzca un número entero positivo:");
+        num = sc.nextInt();
+
+        digitos_eliminados = 0;
+        numero_final = 0;
+
+        while (num!=0) {
+            int log = (int) Math.log10(num);
+            cifra = num / (int) Math.pow(10,log);
+
+            if (cifra!=0 && cifra!=8) {
+                numero_final = numero_final*10+cifra;
+            }
+            num = num-cifra*(int) Math.pow(10,log);
+        }
+
+        System.out.println("Número resultado: " + numero_final);
+        System.out.println("Dígitos eliminados: " + digitos_eliminados);
+    }
 }
