4e2b197ec5ce9bdd1af8777b5c53fb19ee711823
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-11-04 16:36:23 +0100

    Ejercicio1_12 Resuelto (Tarea1_Unidad3)

--- U3_Programacion_Modular/PRACTICAS/Tarea 1/src/com/ejercicios03_11_2020/Ejercicio1_12.java
+++ U3_Programacion_Modular/PRACTICAS/Tarea 1/src/com/ejercicios03_11_2020/Ejercicio1_12.java
@@ -1,4 +1,29 @@
 package com.ejercicios03_11_2020;
 
+import java.util.Scanner;
+
 public class Ejercicio1_12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.println("Introduce un Número:");
+        num = sc.nextInt();
+
+        System.out.println(" ");
+
+        System.out.println(num + " no es igual a " + factorial(num));
+    }
+
+    public static int factorial(int num) {
+        int resto;
+
+        if (num == 0) {
+            resto = 1;
+        } else {
+            resto = num * factorial(num - 1);
+        }
+        return (resto);
+    }
+
 }
