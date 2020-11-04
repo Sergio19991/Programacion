8233086f2b04a854875522ac45beac604c993f51
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-11-04 12:39:58 +0100

    Tarea1_U3 Ejercicios9y10 Resueltos

--- U3_Programacion_Modular/PRACTICAS/Tarea 1/src/com/ejercicios03_11_2020/Ejercicio1_10.java
+++ U3_Programacion_Modular/PRACTICAS/Tarea 1/src/com/ejercicios03_11_2020/Ejercicio1_10.java
@@ -1,4 +1,43 @@
 package com.ejercicios03_11_2020;
 
+import java.util.Scanner;
+
 public class Ejercicio1_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.println("Introduce el Primer Número:");
+        num1 = sc.nextInt();
+        System.out.println("Introduce el Segundo Número:");
+        num2 = sc.nextInt();
+
+        if (amigos(num1, num2)) {
+            System.out.println(num1 + " y " + num2 + " son amigos.");
+        } else {
+            System.out.println("No son amigos.");
+        }
+    }
+
+    public static boolean amigos(int num1, int num2) {
+        boolean amigos;
+
+        if (num1 == sumaDivisoresPropios(num2) && num2 == sumaDivisoresPropios(num1)) {
+            amigos = true;
+        } else {
+            amigos = false;
+        }
+        return amigos;
+    }
+
+    public static int sumaDivisoresPropios(int num) {
+        int suma = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                suma += i;
+            }
+        }
+        return (suma);
+    }
+
 }
--- U3_Programacion_Modular/PRACTICAS/Tarea 1/src/com/ejercicios03_11_2020/Ejercicio1_9.java
+++ U3_Programacion_Modular/PRACTICAS/Tarea 1/src/com/ejercicios03_11_2020/Ejercicio1_9.java
@@ -1,4 +1,34 @@
 package com.ejercicios03_11_2020;
 
+import java.util.Scanner;
+
 public class Ejercicio1_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.println("Introduce un Número:");
+        num = sc.nextInt();
+
+        divisoresPrimos(num);
+    }
+
+    public static boolean esPrimo(int num) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void divisoresPrimos(int num) {
+        System.out.println("Divisores primos de " + num);
+        for (int i = 1; i <= num; i++) {
+            if (esPrimo(i) && num % i == 0) {
+                System.out.println("El número " + i);
+            }
+        }
+    }
+
 }
