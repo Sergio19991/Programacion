e610adfebad3e3ea6e0e32b78bc69aa221e57f59
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-11-10 12:09:11 +0100

    Ejercicio 1 Corregido (Tarea2_Unidad3)

--- U3_Programacion_Modular/PRACTICAS/Tarea 2/src/com/ejercicio06_11_2020/Actividad2_1.java
+++ U3_Programacion_Modular/PRACTICAS/Tarea 2/src/com/ejercicio06_11_2020/Actividad2_1.java
@@ -1,7 +1,22 @@
 package com.ejercicio06_11_2020;
 
+import java.util.Scanner;
+
 public class Actividad2_1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String palabra, frase;
 
+        frase = "";
+
+        do {
+            System.out.println("Introduce palabra:");
+            palabra = sc.nextLine();
+            if (!palabra.equals("fin")) {
+                frase += " " + palabra;
+            }
+        } while (!palabra.equals("fin"));
+
+        System.out.println(frase + ".");
     }
 }
