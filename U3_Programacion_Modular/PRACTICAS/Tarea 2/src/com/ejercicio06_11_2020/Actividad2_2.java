7d02d85b431d17f21d3921583297be4a1931850e
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-11-10 12:16:17 +0100

    Ejercicio 2 Corregido (Tarea2_Unidad3)

--- U3_Programacion_Modular/PRACTICAS/Tarea 2/src/com/ejercicio06_11_2020/Actividad2_2.java
+++ U3_Programacion_Modular/PRACTICAS/Tarea 2/src/com/ejercicio06_11_2020/Actividad2_2.java
@@ -1,7 +1,22 @@
 package com.ejercicio06_11_2020;
 
+import java.util.Scanner;
+
 public class Actividad2_2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String frase;
+        int numespacios = 0;
 
+        System.out.println("Introduce la frase:");
+        frase = sc.nextLine();
+
+        for (int i = 0; i < frase.length(); i++) {
+            if (frase.charAt(i) == ' ') {
+                numespacios++;
+            }
+        }
+
+        System.out.println("Hay " + numespacios + " espacios.");
     }
 }
