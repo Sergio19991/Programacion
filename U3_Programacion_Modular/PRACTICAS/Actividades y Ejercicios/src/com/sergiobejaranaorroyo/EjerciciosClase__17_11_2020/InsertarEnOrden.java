622eca54af03f0b7d504ead3831e07699f69f528
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2020-11-19 20:18:30 +0100

    Insertar en Orden

--- U3_Programacion_Modular/PRACTICAS/Actividades y Ejercicios/src/com/sergiobejaranaorroyo/EjerciciosClase__17_11_2020/InsertarEnOrden.java
+++ U3_Programacion_Modular/PRACTICAS/Actividades y Ejercicios/src/com/sergiobejaranaorroyo/EjerciciosClase__17_11_2020/InsertarEnOrden.java
@@ -1,26 +1,42 @@
 package com.sergiobejaranaorroyo.EjerciciosClase__17_11_2020;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class InsertarEnOrden {
     public static void main(String[] args) {
-        int[] b = {2, 5, 7, 19, 21};
+        int[] vector = {1, 2, 3, 7, 5, 6};
 
-        System.out.println(Arrays.toString(insertarEnOrden(b, 17)));
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        System.out.println("Introduce un Número:");
+        numero = sc.nextInt();
+        System.out.println(" ");
+
+        vector = insertarOrden(numero, vector);
+
+        System.out.println("El Vector Ordenado y con " + numero + ", quedaría     --->     " + Arrays.toString(vector));
     }
 
-    public static int[] insertarEnOrden(int[] v, int num) {
-        int[] resultado = new int[v.length + 1];
+    public static int[] insertarOrden(int num, int[] v) {
+        int[] posicion = new int[v.length + 1];
+
+        Arrays.sort(v);
+
+        int contador = 0;
 
         for (int i = 0; i < v.length; i++) {
-            if (v[i] < num) {
-                resultado[i] = v[i];
+            if (num > v[i]) {
+                posicion[i] = v[i];
+                contador++;
             } else {
-                resultado[i] = num;
+                posicion[i + 1] = v[i];
             }
         }
 
-        return resultado;
+        posicion[contador] = num;
+
+        return posicion;
     }
 
 }
