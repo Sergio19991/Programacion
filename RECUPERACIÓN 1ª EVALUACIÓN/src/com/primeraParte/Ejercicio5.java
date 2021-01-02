ac1164be04767a59858b9ba8c0ed4c7179bb0e30
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2021-01-02 17:39:44 +0100

    Ejercicio 5 (Primera Parte); Recuperación

--- RECUPERACIÓN 1ª EVALUACIÓN/src/com/primeraParte/Ejercicio5.java
+++ RECUPERACIÓN 1ª EVALUACIÓN/src/com/primeraParte/Ejercicio5.java
@@ -1,4 +1,22 @@
 package com.primeraParte;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short contadorPrimos = 0;
+        int num;
+
+        System.out.print("Por favor, introduzca un número entero positivo: ");
+        num = sc.nextInt();
+
+        for (int i = 0; i < (num + 1); i++) {
+            if ((i % 2 != 0 && i != 1) || (i == 2)) {
+                contadorPrimos++;
+            }
+        }
+
+        System.out.println(contadorPrimos);
+    }
 }
