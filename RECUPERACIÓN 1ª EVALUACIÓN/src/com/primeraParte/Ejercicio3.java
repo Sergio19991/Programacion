6be580172e5179709a4ac85f4445dd7b8ed71c19
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2021-01-02 16:11:04 +0100

    Ejercicio 3 (Primera Parte); Recuperación

--- RECUPERACIÓN 1ª EVALUACIÓN/src/com/primeraParte/Ejercicio3.java
+++ RECUPERACIÓN 1ª EVALUACIÓN/src/com/primeraParte/Ejercicio3.java
@@ -1,4 +1,42 @@
 package com.primeraParte;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduzca la altura de la U: ");
+        int altura = sc.nextInt();
+
+        if (altura > 3) {
+            for (int i = 0; i < altura - 1; i++) {
+                System.out.print("*");
+
+                for (int j = 0; j < 5; j++) {
+                    System.out.print(" ");
+                }
+
+                System.out.println("*");
+            }
+
+            for (int i = 0; i < 3; i++) {
+                System.out.print(" *");
+            }
+        } else {
+            for (int i = 0; i < altura - 1; i++) {
+                System.out.print("*");
+
+                for (int j = 0; j < 5; j++) {
+                    System.out.print(" ");
+                }
+
+                System.out.println("*");
+            }
+
+            for (int i = 0; i < 3; i++) {
+                System.out.print(" *");
+            }
+        }
+    }
 }
