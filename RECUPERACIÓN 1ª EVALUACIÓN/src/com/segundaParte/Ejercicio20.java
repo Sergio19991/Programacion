5d872535ae52e55ada7e77a55c7635b31445edbc
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2021-01-04 14:25:09 +0100

    Ejercicio 20 (Segunda Parte); Recuperación

--- RECUPERACIÓN 1ª EVALUACIÓN/src/com/segundaParte/Ejercicio20.java
+++ RECUPERACIÓN 1ª EVALUACIÓN/src/com/segundaParte/Ejercicio20.java
@@ -1,4 +1,36 @@
 package com.segundaParte;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ejercicio20 {
+    public static void main(String[] args) {
+        System.out.println();
+        int numeros[] = new int[5];
+        llenarArray(numeros);
+        visualizar(numeros);
+        System.out.println("\nArray: ");
+        ocurrencias(numeros);
+    }
+
+    static void visualizar(int[] pArray) {
+        System.out.println(Arrays.toString(pArray));
+    }
+
+    static void llenarArray(int[] pArray) {
+        Random r = new Random();
+        for (int i = 0; i < pArray.length; i++) {
+            pArray[i] = r.nextInt(20) + 1;
+        }
+    }
+
+    static void ocurrencias(int[] pArray) {
+        for (int i = 0; i < pArray.length - 1; i++) {
+            for (int j = i + 1; j < pArray.length; j++) {
+                if (pArray[i] == pArray[j]) {
+                    System.out.println("Número repetido: " + pArray[i]);
+                }
+            }
+        }
+    }
 }
