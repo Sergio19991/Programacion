bd89b61f11ea8dc57f2fb2f391a0f4857deccf43
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2021-01-04 14:17:51 +0100

    Ejercicio 16 (Primera Parte); Recuperación

--- RECUPERACIÓN 1ª EVALUACIÓN/src/com/segundaParte/Ejercicio16.java
+++ RECUPERACIÓN 1ª EVALUACIÓN/src/com/segundaParte/Ejercicio16.java
@@ -1,4 +1,26 @@
 package com.segundaParte;
 
+import java.util.Scanner;
+
 public class Ejercicio16 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduce la altura del triángulo: ");
+
+        int altura = teclado.nextInt();
+
+        for (int fila = 1; fila <= altura; fila++) {
+            System.out.println("");
+
+            for (int col = 1; col <= fila; col++) {
+                if ((col == 1) || (fila == col) || (fila == altura)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+        }
+
+        System.out.println();
+    }
 }
