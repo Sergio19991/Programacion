cd7dad90e67644c321e51b337484ff3966deeff7
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2021-01-04 14:12:17 +0100

    Ejercicio 14 (Primera Parte); Recuperación

--- RECUPERACIÓN 1ª EVALUACIÓN/src/com/segundaParte/Ejercicio14.java
+++ RECUPERACIÓN 1ª EVALUACIÓN/src/com/segundaParte/Ejercicio14.java
@@ -1,4 +1,52 @@
 package com.segundaParte;
 
+import java.util.Scanner;
+
 public class Ejercicio14 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int num = teclado.nextInt();
+        int[] n = new int[num];
+        for (int i = 0; i < 0; i++) {
+            n[i] = teclado.nextInt();
+        }
+        System.out.print("El número en código morse es: " + convierteEnMorse(num));
+    }
+
+    public static int convierteEnMorse(int n) {
+        switch (n) {
+            case 1:
+                System.out.println(". _ _ _ _");
+                break;
+            case 2:
+                System.out.println(".. _ _ _");
+                break;
+            case 3:
+                System.out.println("... _ _");
+                break;
+            case 4:
+                System.out.println("...._");
+                break;
+            case 5:
+                System.out.println(".....");
+                break;
+            case 6:
+                System.out.println("_....");
+                break;
+            case 7:
+                System.out.println("_ _...");
+                break;
+            case 8:
+                System.out.println("_ _ _..");
+                break;
+            case 9:
+                System.out.println("_ _ _ _.");
+                break;
+            case 0:
+                System.out.println("_ _ _ _ _");
+                break;
+        }
+        return n;
+    }
 }
