87156bd3acedee8a5365c75b18c037d0987eecde
Sergio19991 <alum.sbejaranoa.2020@iesalixar.org>
2021-01-04 14:18:57 +0100

    Ejercicio 17 (Primera Parte); Recuperación

--- RECUPERACIÓN 1ª EVALUACIÓN/src/com/segundaParte/Ejercicio17.java
+++ RECUPERACIÓN 1ª EVALUACIÓN/src/com/segundaParte/Ejercicio17.java
@@ -1,4 +1,16 @@
 package com.segundaParte;
 
 public class Ejercicio17 {
+    public static void main(String[] args) {
+        int[] num = {1, 5, 9, 3, 4, 6};
+        System.out.println(convierteArrayEnString(num));
+    }
+
+    public static String convierteArrayEnString(int[] a) {
+        String cadena = "";
+        for (int i = 0; i < a.length; i++) {
+            cadena = cadena + a[i];
+        }
+        return cadena;
+    }
 }
